c8e576a92e1534e7cf51b204f0549320ee756e72
thuyanduong-flatiron <thuyanannduong@gmail.com>
2019-04-24 20:32:05 -0400

    added solution

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+function fetchFourDogs(){
+  fetch(imgUrl)
+  .then(res => res.json())
+  .then(json => {
+    json.message.forEach(dogImageURL => {
+      let imageElement = document.createElement('img')
+      imageElement.src = dogImageURL
+      document.querySelector('#dog-image-container').appendChild(imageElement)
+    })
+  })
+}
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+function fetchBreeds(filter=undefined){
+  let container = document.querySelector('#dog-breeds')
+  container.innerHTML = ''
+  fetch(breedUrl)
+  .then(res => res.json())
+  .then(json => {
+    for(breed in json.message){
+      if(!filter || breed.startsWith(filter)){
+        let breedElement = document.createElement('li')
+        breedElement.innerText = breed
+        container.appendChild(breedElement)
+        breedElement.addEventListener('click', handleChangeColor)
+      }
+    }
+  })
+}
+
+function handleChangeColor(event){
+  event.target.style.color = 'red'
+}
+
+function handleDropDown(event){
+  fetchBreeds(event.target.value)
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  fetchFourDogs()
+  fetchBreeds()
+  document.querySelector('#breed-dropdown').addEventListener('change', handleDropDown)
+})
